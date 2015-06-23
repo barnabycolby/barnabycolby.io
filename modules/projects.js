094fedd4ed43e4dd13cb2aa79de593f590d27776
Barnaby Colby <barneycolby@gmail.com>
2015-06-23 21:14:23 +0000

    Added projects directive so that I can write the html for each project in one place.

--- modules/projects.js
+++ modules/projects.js
@@ -1,3 +1,15 @@
 (function () {
   var app = angular.module('projects', ['header']);
+
+  app.directive('projects', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'snippets/projects.html',
+      controller: function () {
+        this.projects = [
+        ];
+      },
+      controllerAs: 'projectsController'
+    };
+  });
 })()
