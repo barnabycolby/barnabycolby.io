67e58a293252d73628161e1d5bfc45d72268b550
Barnaby Colby <barneycolby@gmail.com>
2015-06-23 21:26:33 +0000

    Encapsulated the JSON for the projects in a file, which keeps the code clean and makes it easy to add new projects.

--- modules/projects.js
+++ modules/projects.js
@@ -1,15 +1,17 @@
 (function () {
   var app = angular.module('projects', ['header']);
 
-  app.directive('projects', function() {
+  app.directive('projects', ['$http', function($http) {
     return {
       restrict: 'E',
       templateUrl: 'snippets/projects.html',
       controller: function () {
-        this.projects = [
-        ];
+        var projectsController = this;
+        $http.get('/data/projects.json').success(function (data) {
+          projectsController.projects = data;
+        });
       },
       controllerAs: 'projectsController'
     };
-  });
+  }]);
 })()
