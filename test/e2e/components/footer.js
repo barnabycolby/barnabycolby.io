ab9ffa0560beb9b76a78ede1cb2a2186e609d868
Barnaby Colby <barnaby.colby@gmail.com>
2015-09-16 19:56:58 +0000

    Added remaining e2e tests for footer contact buttons.

--- test/e2e/components/footer.js
+++ test/e2e/components/footer.js
@@ -12,6 +12,16 @@
                     var emailElement = element(by.css('a[href="mailto:barnaby.colby@gmail.com"]'));
                     expect(emailElement.isPresent()).toBe(true);
                 });
+
+                it('should contain a link to my linkedin profile', function () {
+                    var linkedinElement = element(by.css('a[href="https://uk.linkedin.com/in/barneycolby"]'));
+                    expect(linkedinElement.isPresent()).toBe(true);
+                });
+
+                it('should contain a link to my github profile', function () {
+                    var githubElement = element(by.css('a[href="https://github.com/barnabycolby"]'));
+                    expect(githubElement.isPresent()).toBe(true);
+                });
             });
         }
     };
