7629ca8f4b1e8826d7dcb0b7c05e012457b7156f
Barnaby Colby <barnaby.colby@gmail.com>
2015-09-18 10:36:19 +0000

    Refactored tests to encapsulate common functionality between them, and also allow changes to data schemas easier to deal with.

--- test/unit/projects.spec.js
+++ test/unit/projects.spec.js
@@ -5,7 +5,7 @@
 (function () {
     'use strict';
     describe('The projects element directive', function () {
-        var $compile, $rootScope, $httpBackend;
+        var $compile, $rootScope, $httpBackend, testData, compileElement;
 
         // Load the project module, which contains the projects directive
         beforeEach(module('projects'));
@@ -21,33 +21,32 @@
             $httpBackend = $injector.get('$httpBackend');
         }));
 
-        it('does not expand to anything when projects.json is empty', function () {
-            $httpBackend.whenGET('/data/projects.json').respond(200, '');
+        testData = '[{"name":"Website"},{"name":"Backup Server"},{"name":"VPN Server"}]';
 
+        compileElement = function () {
             var element = $compile("<projects></projects>")($rootScope);
             $rootScope.$digest();
             $httpBackend.flush();
 
+            return element;
+        };
+
+        it('does not expand to anything when projects.json is empty', function () {
+            $httpBackend.whenGET('/data/projects.json').respond(200, '');
+            var element = compileElement();
             expect(element.find('h3').length).toBe(0);
         });
 
         it('does not expand to anything when projects.json contains an empty array', function () {
             $httpBackend.whenGET('/data/projects.json').respond(200, '[]');
-
-            var element = $compile("<projects></projects>")($rootScope);
-            $rootScope.$digest();
-            $httpBackend.flush();
-
+            var element = compileElement();
             expect(element.find('h3').length).toBe(0);
         });
 
         it('creates a h3 element for each object in the array with the correct text', function () {
             var element, h3s;
-            $httpBackend.whenGET('/data/projects.json').respond(200, '[{"name":"Website"},{"name":"Backup Server"},{"name":"VPN Server"}]');
-
-            element = $compile("<projects></projects>")($rootScope);
-            $rootScope.$digest();
-            $httpBackend.flush();
+            $httpBackend.whenGET('/data/projects.json').respond(200, testData);
+            element = compileElement();
 
             h3s = element.find('h3');
             expect(h3s.length).toBe(3);
@@ -55,5 +54,11 @@
             expect(h3s.eq(1).text()).toBe('Backup Server');
             expect(h3s.eq(2).text()).toBe('VPN Server');
         });
+
+        /*
+        it('creates a p element for each object in the array with the correct description', function () {
+        
+        });
+        */
     });
 }());
