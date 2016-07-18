91613415267e1773b51c97fe9289ca2f73242dd0
Barnaby Colby <barnaby.colby@gmail.com>
2016-07-18 18:08:50 +0000

    Temporarily removed the grunt uncss task from the pipeline as it seems to be broken when using URLs as sources, as described in #33.

--- grunt/less.js
+++ grunt/less.js
@@ -9,6 +9,8 @@
         },
         main: {
             files: {
+                // Temporarily changed the destination from custom.css to stylesheet.css, allowing us to bypass the uncss step
+                // As uncss is broken right now (doesn't handle urls as sources)
                 '<%= destinationWorkingDirectory %>/css/custom.css': '<%= sourceWorkingDirectory %>/less/stylesheet.less'
             }
         }
