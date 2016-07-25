ae59ac7997cd9e5a014434e8e12c2e26ce4d7bb2
Barnaby Colby <barnaby.colby@gmail.com>
2016-07-25 22:21:40 +0000

    Updated footer and header component tests to use the helper version of the existsAndIsVisible function, which completes its encapsulation.

--- test/e2e/components/footer.js
+++ test/e2e/components/footer.js
@@ -3,23 +3,22 @@
     'use strict';
 
     var FooterTest = function () {
-        var existsAndIsVisible = function (selector) {
-            expect(browser.isExisting(selector)).toBe(true);
-            expect(browser.isVisible(selector)).toBe(true);
-        }
+        var Helper, helper;
+        Helper = require('../helper.js');
+        helper = new Helper();
 
         this.test = function () {
             describe("The footer", function () {
                 it('should contain a link to my email address', function () {
-                    existsAndIsVisible('#footer a[href="mailto:barnaby.colby@gmail.com"]');
+                    helper.existsAndIsVisible('#footer a[href="mailto:barnaby.colby@gmail.com"]');
                 });
 
                 it('should contain a link to my linkedin profile', function () {
-                    existsAndIsVisible('#footer a[href="https://uk.linkedin.com/in/barnabycolby"]');
+                    helper.existsAndIsVisible('#footer a[href="https://uk.linkedin.com/in/barnabycolby"]');
                 });
 
                 it('should contain a link to my github profile', function () {
-                    existsAndIsVisible('#footer a[href="https://github.com/barnabycolby"]');
+                    helper.existsAndIsVisible('#footer a[href="https://github.com/barnabycolby"]');
                 });
 
                 it("should contain copyright text", function () {
--- test/e2e/components/header.js
+++ test/e2e/components/header.js
@@ -3,16 +3,15 @@
     'use strict';
 
     var HeaderTest = function () {
-        var existsAndIsVisible = function (selector) {
-            expect(browser.isExisting(selector)).toBe(true);
-            expect(browser.isVisible(selector)).toBe(true);
-        }
+        var Helper, helper;
+        Helper = require('../helper.js');
+        helper = new Helper();
 
         this.test = function () {
             describe("The header", function () {
                 it('should contain my name as a link to the homepage', function () {
                     var selector = '#header a[href="/"]';
-                    existsAndIsVisible(selector);
+                    helper.existsAndIsVisible(selector);
                     expect(browser.getText(selector)).toBe('Barnaby Colby');
                 });
             });
@@ -24,7 +23,7 @@
             describe("navigation bar", function () {
                 it('should contain a link to the projects page', function () {
                     var selector = '#navigation li a[href="/projects.html"]';
-                    existsAndIsVisible(selector);
+                    helper.existsAndIsVisible(selector);
                     expect(browser.getText(selector)).toBe('Projects');
                 });
             });
