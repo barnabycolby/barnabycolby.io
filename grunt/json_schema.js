00c3bc07046e3cdccbe3643d2f6eb2deceaeb09f
Barnaby Colby <barnaby.colby@gmail.com>
2016-07-25 10:56:05 +0000

    Improved json_schema grunt config file so that each json file in the directory is automatically checked.

--- grunt/json_schema.js
+++ grunt/json_schema.js
@@ -7,20 +7,11 @@
         options: {
             validateFormatsStrict: true
         },
-        projects: {
-            files: {
-                '<%= sourceWorkingDirectory %>/schemas/projects.json': '<%= sourceWorkingDirectory %>/data/projects.json'
-            }
-        },
-        navigation: {
-            files: {
-                '<%= sourceWorkingDirectory %>/schemas/navigation.json': '<%= sourceWorkingDirectory %>/data/navigation.json'
-            }
-        },
-        contactButtons: {
-            files: {
-                '<%= sourceWorkingDirectory %>/schemas/contactButtons.json': '<%= sourceWorkingDirectory %>/data/contactButtons.json'
-            }
+        main: {
+            expand: true,
+            cwd: '<%= sourceWorkingDirectory %>/data',
+            src: ['*.json'],
+            dest: '<%= sourceWorkingDirectory %>/schemas'
         }
 
     };
