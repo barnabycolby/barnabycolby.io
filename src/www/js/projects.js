2f17870f0c6ae497554488a3390d48d21a9fb71f
Barnaby Colby <barnaby.colby@gmail.com>
2016-08-01 09:44:54 +0000

    Added support for anonymous contributors and repositories with more than 30 contributors to the GitHub integration.

--- src/www/js/projects.js
+++ src/www/js/projects.js
@@ -46,26 +46,43 @@
         // In order to query the GitHub API We require the owner and repository name, which can be extracted from the github link
         // More precisely, we transform the old link
         apiLink = githubLink.replace('github.com', 'api.github.com/repos');
-        apiLink += '/contributors';
+        apiLink += '/contributors?anon=true';
 
-        // Next, we retrive the list of contributors by following the link
-        getJSON(apiLink, function (err, data) {
-            var i, commitTotal = 0, commitTotalElement;
-            if (err !== null) {
-                hideGitHubIntegrations();
-                return;
-            }
+        // This function gets the total number of commits using the given api link
+        // Once it has the final total, it updates the webpage accordingly
+        // It needs to be defined as a function because it calls itself recursively
+        (function getAndSetCommitTotal(baseApiLink, commitTotalSoFar, startingPageNumber) {
+            // We need to set default values for commitTotalSoFar and startingPageNumber if they have not been given
+            commitTotalSoFar = commitTotalSoFar === undefined ? 0 : commitTotalSoFar;
+            startingPageNumber = startingPageNumber === undefined ? 1 : startingPageNumber;
 
-            // To get the total commit count, we sum the number of commits from every contributor
-            for (i = 0; i < data.length; i += 1) {
-                commitTotal += data[i].contributions;
-            }
+            // To handle the GitHub API pagination functionality, we need to append a page number to the link
+            var paginatedApiLink = baseApiLink + "&page=" + startingPageNumber;
 
-            // Display the commit total in the appropriate element, and hide the loading spinner
-            commitTotalElement = document.getElementById(numberOfCommitsId);
-            commitTotalElement.innerHTML = commitTotal + ' Commits';
-            document.getElementById(githubSpinnerId).style.display = 'none';
-            commitTotalElement.style.display = 'block';
-        });
+            getJSON(paginatedApiLink, function (err, data) {
+                var i, commitTotalElement;
+                if (err !== null) {
+                    hideGitHubIntegrations();
+                    return;
+                }
+
+                // To get the total commit count, we sum the number of commits from every contributor
+                for (i = 0; i < data.length; i += 1) {
+                    commitTotalSoFar += data[i].contributions;
+                }
+
+                // GitHub limits the response to contain at most 30 objects (see https://developer.github.com/v3/#pagination)
+                // We need to check that there isn't more pages of contributors
+                if (data.length === 30) {
+                    getAndSetCommitTotal(baseApiLink, commitTotalSoFar, startingPageNumber + 1);
+                } else {
+                    // Display the commit total in the appropriate element, and hide the loading spinner
+                    commitTotalElement = document.getElementById(numberOfCommitsId);
+                    commitTotalElement.innerHTML = commitTotalSoFar + ' Commits';
+                    document.getElementById(githubSpinnerId).style.display = 'none';
+                    commitTotalElement.style.display = 'block';
+                }
+            });
+        }(apiLink));
     };
 }());
